9c38fb41f2d013de02e52baf23fa82562a2fa6f4
piotrfatner <fatner.piotr@gmail.com>
2017-04-19 23:39:59 +0200

    client server completed

--- src/main/java/wdsr/exercise3/record/rest/RecordResource.java
+++ src/main/java/wdsr/exercise3/record/rest/RecordResource.java
@@ -1,12 +1,18 @@
 package wdsr.exercise3.record.rest;
 
 import javax.inject.Inject;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 
+import com.sun.org.apache.regexp.internal.RE;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import wdsr.exercise3.record.Record;
 import wdsr.exercise3.record.RecordInventory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/records")
 public class RecordResource {
@@ -28,6 +34,14 @@ public class RecordResource {
 	 * ** Returns a list of all records (as application/xml)
 	 * ** Response status: HTTP 200
 	 */
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Response getRecords(){
+		List<Record> records = new ArrayList<>();
+		records.addAll(recordInventory.getRecords());
+		GenericEntity<List<Record>> genericResult = new GenericEntity<List<Record>>(records) {};
+		return Response.ok(genericResult).build();
+	}
 
 	/**
 	 * POST https://localhost:8091/records
@@ -36,13 +50,38 @@ public class RecordResource {
 	 * ** Response status if ok: HTTP 201, Location header points to the newly created resource.
 	 * ** Response status if submitted record has ID set: HTTP 400
 	 */
-	
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_XML)
+	public Response addRecord(Record record, @Context UriInfo uriInfo){
+		if(record.getId() != null){
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+		recordInventory.addRecord(record);
+		UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+		builder.path(Integer.toString(record.getId()));
+
+		return Response.created(builder.build()).build();
+	}
+
 	/**
 	 * * GET https://localhost:8091/records/{id}
 	 * ** Returns an existing record (as application/xml)
 	 * ** Response status if ok: HTTP 200
 	 * ** Response status if {id} is not known: HTTP 404
 	 */
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Response getRecord(@PathParam(value = "id") int id){
+		Record record = recordInventory.getRecord(id);
+		if(record == null){
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		GenericEntity<Record> genericResult = new GenericEntity<Record>(record) {};
+		return Response.ok(genericResult).build();
+	}
+
 	 /**
 	 * * PUT https://localhost:8091/records/{id}
 	 * ** Replaces an existing record in entirety.
@@ -50,6 +89,18 @@ public class RecordResource {
 	 * ** Response status if ok: HTTP 204
 	 * ** Response status if {id} is not known: HTTP 404
 	 */
+	@PUT
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Response updateRecord(Record record,@PathParam(value = "id") int id){
+		if (record.getId() != null && id != record.getId()) {
+			return Response.status(Response.Status.BAD_REQUEST).entity("Product ID is different in request path and message body").build();
+		}
+		if(recordInventory.updateRecord(id,record)){
+			return Response.noContent().build();
+		}
+		return Response.status(Response.Status.NOT_FOUND).build();
+	}
 	
 	/**
 	 * * DELETE https://localhost:8091/records/{id}
@@ -57,4 +108,14 @@ public class RecordResource {
 	 * ** Response status if ok: HTTP 204
 	 * ** Response status if {id} is not known: HTTP 404
 	 */
+	@DELETE
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_XML)
+	public Response deleteProduct(@PathParam(value = "id") int id) {
+		if(recordInventory.deleteRecord(id)){
+			return Response.noContent().build();
+		}
+		return Response.status(Response.Status.NOT_FOUND).build();
+	}
 }
